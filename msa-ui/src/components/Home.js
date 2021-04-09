74745b5454cb2779fe6751260e2360aaea2bd3e6
Luke Mainwaring <lukemainwaring@Lukes-MBP.lan>
2021-04-09 11:38:32 -0400

    include basic bpm and key selection on homepage

--- msa-ui/src/components/Home.js
+++ msa-ui/src/components/Home.js
@@ -1,17 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Typography, withStyles } from '@material-ui/core';
+import {
+  Typography,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { VALID_SONG_KEYS } from '../util/constants';
 
 const Home = ({}) => {
+  const classes = useStyles();
+
+  const [tempo, setTempo] = useState('120');
+  const [songKey, setSongKey] = useState('C major');
+
+  const handleTempoChange = (e) => {
+    setTempo(e.target.value);
+  };
+
+  const handleSongKeyChange = (e) => {
+    setSongKey(e.target.value);
+  };
+
   return (
     <div>
-      <Typography variant='h2' component='h2' align='center'>
-        Welcome to Music Sample Assistant
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant='h2' component='h2' align='center'>
+            Welcome to Music Sample Assistant
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify='center'>
+            <Grid item>
+              <Typography variant='h5' component='h5'>
+                Select Tempo
+              </Typography>
+            </Grid>
+            <Grid item className={classes.bpmInput}>
+              <TextField
+                required
+                id='standard-required'
+                label='Input Tempo'
+                name='tempo'
+                value={tempo}
+                onChange={handleTempoChange}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify='center'>
+            <Grid item>
+              <Typography variant='h5' component='h5'>
+                Select Key
+              </Typography>
+            </Grid>
+            <Grid item className={classes.bpmInput}>
+              <InputLabel id='demo-simple-select-label'>Song Key</InputLabel>
+              <Select
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
+                name='songKey'
+                value={songKey}
+                onChange={handleSongKeyChange}
+              >
+                {VALID_SONG_KEYS.map((songKey, index) => (
+                  <MenuItem key={index} value={songKey}>
+                    {songKey}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Typography variant='h4' component='h4' align='center'>
+        (eventually) upload current song section
       </Typography>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  bpmInput: {
+    marginBottom: 20,
+    marginLeft: 20,
+  },
+}));
 
 const mapStateToProps = ({}) => {
   return {};
@@ -21,9 +102,4 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({}, dispatch);
 };
 
-const styles = {};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ msa-ui/src/util/constants.js
@@ -0,0 +1,11 @@
+export const VALID_SONG_KEYS = [
+  'A major',
+  'B major',
+  'C major',
+  'D major',
+  'E major',
+  'A minor',
+  'B minor',
+  'C minor',
+  'D minor',
+];
